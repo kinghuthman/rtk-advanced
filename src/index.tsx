dcbbca1b3d1791f74773bf09593733fa314d7c56
kinghuthman <kinghuthman@gmail.com>
2020-11-11 21:14:18 -0600

    render redux provider with hot module reload

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import './index.css'
-import App from './app/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import store from './app/store'
+
+const render = () => {
+  const App = require('./app/App').default
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  )
+}
+
+render()
+
+/** to hot reload components, import <App> in a function, call it once on startup
+ * to show the react component tree, then reuse that function any time a component is changed
+ */
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./app/App', render)
+}
