d2108681ca8ec59803097740c5faa30885a55a3b
kinghuthman <kinghuthman@gmail.com>
2020-11-11 20:26:43 -0600

    setup store and hot reload

---
+++ src/app/store.ts
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import rootReducer from './rootreducer'
+
+const store = configureStore({
+  reducer: rootReducer
+})
+
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  // CommonJS synchronous import syntax
+  module.hot.accept('./rootReducer', () => {
+    // "default export" is in an object field called default
+    const newRootReducer = require('./rootReducer').default
+    store.replaceReducer(newRootReducer)
+  })
+}
+
+export type AppDispatch = typeof store.dispatch
+
+export default store
