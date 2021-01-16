b41629800ea71a0f5f675d30c4742cbcc8c43ab2
kinghuthman <kinghuthman@gmail.com>
2021-01-15 18:21:42 -0600

    slice to track comments

--- src/app/rootreducer.ts
+++ src/app/rootreducer.ts
@@ -3,11 +3,13 @@ import { combineReducers } from '@reduxjs/toolkit'
 import issuesDisplayReducer from 'features/issuesDisplay/issuesDisplaySlice'
 import repoDetailsReducer from 'features/repoSearch/repoDetailsSlice'
 import issuesReducer from 'features/issuesList/issuesSlice'
+import commentsReducer from 'features/issueDetails/commentsSlice'
 
 const rootReducer = combineReducers({
   issuesDisplay: issuesDisplayReducer,
   repoDetails: repoDetailsReducer,
-  issues: issuesReducer
+  issues: issuesReducer,
+  comments: commentsReducer
 })
 
 /* whatever gets returned from our root reducer, our type for whenever we 
